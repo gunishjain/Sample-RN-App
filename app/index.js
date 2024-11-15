47688128547d1deac343a2efae2e8e9b0657e83c
gunishjain <gunish.jain@gmail.com>
2024-11-15 12:58:12 +0530

    Implement Basic Navigation Structure with Bottom Tabs

--- app/index.js
+++ app/index.js
@@ -6,9 +6,9 @@ import { Link } from 'expo-router'
 export default function App(){
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl">Hello DEWDS</Text>
+            <Text className="text-5xl font-pblack">Hello DEWDS</Text>
             <StatusBar style='auto'/>
-            <Link href="/profile" style={{color: 'blue'}}>Go To Profile</Link>
+            <Link href="/home" style={{color: 'blue'}}>Go To Home</Link>
         </View>
     );
 }
